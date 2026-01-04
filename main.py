77c8ec673a36eecebf790f78b68c20830edda78b
arkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2026-01-04 17:40:06 +0100

    Add fast fail when checking for environment value in db-check endpoint

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from typing import Union
-from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
 
+load_dotenv()
 app = FastAPI()
 
 @app.get("/")
@@ -9,4 +11,9 @@ def read_root():
 
 @app.get("/db-check")
 def read_db_version():
-    return {"PostgreSQL version": 0.0}
+    if os.getenv("DB_HOST") is None or os.getenv("DB_HOST") == "":
+        raise HTTPException(status_code=400, detail="DB_HOST environment variable is missing or does not exist")
+    if os.getenv("DB_PASSWORD") is None or os.getenv("DB_PASSWORD") == "":
+        raise HTTPException(status_code=400, detail="DB_PASSWORD environment variable is missing or does not exist")
+
+    return {"PostgreSQL version": 0.0, "Host": os.getenv("DB_HOST"), "Password": os.getenv("DB_PASSWORD")}

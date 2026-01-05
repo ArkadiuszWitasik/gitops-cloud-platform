c699b5c8816258fbbd49fb6a7780edc0fe207cc2
arkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2026-01-05 20:33:54 +0100

    Install psycopg2 package and modify db-check endpoint to retrive pg version

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 
@@ -11,9 +12,20 @@ def read_root():
 
 @app.get("/db-check")
 def read_db_version():
+    conn = None
     if os.getenv("DB_HOST") is None or os.getenv("DB_HOST") == "":
         raise HTTPException(status_code=400, detail="DB_HOST environment variable is missing or does not exist")
     if os.getenv("DB_PASSWORD") is None or os.getenv("DB_PASSWORD") == "":
         raise HTTPException(status_code=400, detail="DB_PASSWORD environment variable is missing or does not exist")
+    try:
+        conn = psycopg2.connect(dbname="postgres", user="postgres", password=os.getenv("DB_PASSWORD"), host=os.getenv("DB_HOST"))
+        cursor = conn.cursor()
+        cursor.execute("SELECT version()")
+        db_version = cursor.fetchone()
+    except (Exception, psycopg2.DatabaseError) as error:
+        raise HTTPException(status_code=400, detail=error)
+    finally:
+        if conn is not None:
+            conn.close()
 
-    return {"PostgreSQL version": 0.0, "Host": os.getenv("DB_HOST"), "Password": os.getenv("DB_PASSWORD")}
+    return {"PostgreSQL version": db_version}

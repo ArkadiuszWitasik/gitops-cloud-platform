35319a60748fe2ac3c5936eb46be14a685c09c50
arkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2026-01-15 22:06:32 +0100

    Create postgre database with AWS RDS and apply AWS Secrets Manager for db master password

---
+++ terraform/database.tf
@@ -0,0 +1,27 @@
+data "aws_secretsmanager_secret" "dev-db-postgre-meta" {
+  name = "dev-db-postgre-pwd"
+}
+
+data "aws_secretsmanager_secret_version" "dev-db-postgre-value" {
+  secret_id = data.aws_secretsmanager_secret.dev-db-postgre-meta.id
+}
+
+resource "aws_db_subnet_group" "postgre" {
+  name = "dev-db"
+  subnet_ids = [ aws_subnet.private_zone1.id, aws_subnet.private_zone2.id ]
+
+  tags = {
+    Name = "dev-db"
+  }
+}
+
+resource "aws_db_instance" "postgre" {
+  allocated_storage    = 5
+  engine               = "postgres"
+  engine_version       = "11"
+  instance_class       = "db.t3.micro"
+  username             = "postgre"
+  password             = jsondecode(data.aws_secretsmanager_secret_version.dev-db-postgre-value.secret_string)["dev-db-postgre-pwd"]
+  db_subnet_group_name = aws_db_subnet_group.postgre.name
+  skip_final_snapshot  = true
+}

e7951ef66810065671d15c8220e5f800f3d428cc
arkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2026-02-05 21:48:30 +0100

    Add security rules for load balancer, webapp and database

--- terraform/security.tf
+++ terraform/security.tf
@@ -24,3 +24,48 @@ resource "aws_security_group" "database" {
     Name = "dev-database-security-group"
   }
 }
+
+resource "aws_security_group_rule" "lb_ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lb.id
+}
+
+resource "aws_security_group_rule" "lb_egress_to_webapp" {
+  type                     = "egress"
+  from_port                = 8000
+  to_port                  = 8000
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.webapp.id
+  security_group_id        = aws_security_group.lb.id
+}
+
+resource "aws_security_group_rule" "webapp_ingress_from_lb" {
+  type                     = "ingress"
+  from_port                = 8000
+  to_port                  = 8000
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lb.id
+  security_group_id        = aws_security_group.webapp.id
+}
+
+resource "aws_security_group_rule" "webapp_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.webapp.id
+}
+
+resource "aws_security_group_rule" "db_ingress_from_webapp" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.webapp.id
+  security_group_id        = aws_security_group.database.id
+}

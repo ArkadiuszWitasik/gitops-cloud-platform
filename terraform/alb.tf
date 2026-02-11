3753e43bfb0d1d3f06673ed304d686fde8bcba90
arkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2026-02-11 21:51:43 +0100

    Create target group and listener for Application Load Balancer

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -8,6 +8,38 @@ resource "aws_alb" "main" {
   }
 }
 
-#TODO: 
-# 1. ALB target group
-# 2. ABL listener
+resource "aws_alb_target_group" "main_lb_target_group" {
+  name        = "main_lb_target_group"
+  protocol    = "HTTP"
+  port        = 80
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = "3"
+    unhealthy_threshold = "3"
+    interval            = "300"
+    protocol            = "HTTP"
+    matcher             = "200"
+    path                = "/health-check"
+  }
+
+  tags = {
+    Name = "main_lb_target_group"
+  }
+}
+
+resource "aws_alb_listener" "webapp_alb_listener" {
+  load_balancer_arn = aws_alb.main.id
+  port              = 8000
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.main_lb_target_group.id
+    type             = "forward"
+  }
+
+  tags = {
+    Name = "webapp_alb_listener"
+  }
+}

f419407f8802f40dcd2c8654a9f5f0a1a7e724a5
arkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2026-03-25 20:28:03 +0100

    Webapp: Create health-check for service, update requirements.txt and .gitignore

--- web-app/main.py
+++ web-app/main.py
@@ -1,4 +1,5 @@
 import os
+
 import psycopg2
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
@@ -6,19 +7,37 @@ from fastapi import FastAPI, HTTPException
 load_dotenv()
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/health-check")
+def health_check():
+    return {"status": "ok"}
+
 
 @app.get("/db-check")
 def read_db_version():
     conn = None
     if os.getenv("DB_HOST") is None or os.getenv("DB_HOST") == "":
-        raise HTTPException(status_code=400, detail="DB_HOST environment variable is missing or does not exist")
+        raise HTTPException(
+            status_code=400,
+            detail="DB_HOST environment variable is missing or does not exist",
+        )
     if os.getenv("DB_PASSWORD") is None or os.getenv("DB_PASSWORD") == "":
-        raise HTTPException(status_code=400, detail="DB_PASSWORD environment variable is missing or does not exist")
+        raise HTTPException(
+            status_code=400,
+            detail="DB_PASSWORD environment variable is missing or does not exist",
+        )
     try:
-        conn = psycopg2.connect(dbname="postgres", user="postgres", password=os.getenv("DB_PASSWORD"), host=os.getenv("DB_HOST"))
+        conn = psycopg2.connect(
+            dbname="postgres",
+            user="postgres",
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+        )
         cursor = conn.cursor()
         cursor.execute("SELECT version()")
         db_version = cursor.fetchone()
